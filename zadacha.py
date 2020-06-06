8698e8889680374bd1ab110c3e8ffa37870c9273
Artemy Tebaykin <artkot012@gmail.com>
2020-06-06 14:17:13 +0300

    Добавил задачу

---
+++ zadacha.py
@@ -0,0 +1,53 @@
+import copy
+spisok1 = input("Введите списки через пробел:").split()
+#spisok1 = ['abc', '123']
+spisok2 = []
+for i in spisok1:
+    i = [i]
+    spisok2.append(i)
+kol_sp = len(spisok2)
+spisok3 = []
+for a in spisok2:
+    for b in a:
+        for c in b:
+            c = [c]
+            spisok3.append(c)
+spisok_ch = []
+for s in spisok2:
+    for c in s:
+        spisok_ch.append(len(c))
+spisok4 = []
+while spisok_ch != []:
+    pr = spisok_ch[0]
+    spisok4.append(spisok3[0:(pr)])
+    for w in range(pr):
+        spisok3.pop(0)
+    spisok_ch.pop(0)
+print(spisok4)
+spisok5 = []
+x1 = 0
+y1 = 0
+x2 = 1
+y2 = 0
+def speen(x1,y1,x2,y2):
+    aa = ((spisok4[x1][y1][0]) + (spisok4[x2][y2][0]))
+    spisok5.append(aa)
+
+spisok6 = []
+for gg in spisok1:
+    for ss in gg:
+        spisok6.append(ss)
+print(spisok6)
+
+sp6 = copy.deepcopy(spisok6)
+
+for u in range(len(spisok4[0])):
+    sp6.remove([u])
+print(sp6)
+for ii in spisok4[0]:
+    for jj in sp6:
+        aaa = ii[0] + jj
+        aaa = [aaa]
+        spisok5.append(aaa)
+
+print(spisok5)
